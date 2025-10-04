293902fd0bd10bd4535430781e6e7e9167dc0d3e
texe2036 <eliott.texier@hotmail.com>
2025-10-04 12:12:16 -0400

    Added a bit more automation for automatic sort in files for gifs

--- main.py
+++ main.py
@@ -14,14 +14,23 @@ ds = nc.Dataset(file_path)
 from ImageGenerator import ImageGenerator, ImageGeneratorMethod
 
 #print(ds.variables["time"][:])
+#print(ds.variables["lev"][:])
 
-for ind_elev, elevation in tqdm(enumerate(ds.variables["lev"][:])) :
+satellite = "MERRA-2"
+analysed_stuff = "O3"
+constant_value = "lev" # NEED TO CHANGE INDEXES :(
+dependant_value = "time" # NEED T CHANGE GIF NAME
+pseudomatrix = ds.variables[analysed_stuff]
+
+max = pseudomatrix[:].max()
+min = pseudomatrix[:].min()
+
+for ind_elev, elevation in tqdm(enumerate(ds.variables[constant_value][:])) :
     images = []
-    for ind_time, time in enumerate(ds.variables["time"][:]) :
-        IG = image_generator = ImageGenerator()
-        matrix = ds.variables["CO"][ind_time, ind_elev, :, :]
-        image = image_generator.generateFromMatrix(matrix, matrix.shape)
+    for ind_time, time in enumerate(ds.variables[dependant_value][:]) :
+        IG = image_generator = ImageGenerator(method=ImageGeneratorMethod.LOGARITHMIC)
+        matrix = pseudomatrix[ind_time, ind_elev, :, :]
+        image = image_generator.generateFromMatrix(matrix, matrix.shape, max, min)
         #image.save("test"+str(i)+".png")
         images.append(image)
-
-images[0].save("test" + str(elevation) + '.gif', save_all=True, append_images=images, loop=0)
+    images[0].save(satellite +'/' + analysed_stuff + "/elevation-of-" + str(elevation) + '.gif', save_all=True, append_images=images, loop=0)

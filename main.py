6f805142127d29d501fdac6b009b1e938f3c1f5a
texe2036 <eliott.texier@hotmail.com>
2025-10-04 11:38:50 -0400

    Gif

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import netCDF4 as nc
 from tqdm import tqdm
 
-from ImageGenerator import ImageGenerator, ImageGeneratorMethod
-
-
 file_path = 'MERRA-2.nc4'
 ds = nc.Dataset(file_path)
 #print(ds)
@@ -13,11 +10,19 @@ ds = nc.Dataset(file_path)
 #print(ds.variables["O3"])
 #print(ds.variables["PS"])
 
-images = []
-for i, time in tqdm(enumerate(ds.variables["time"][:])) :
-    image_generator = ImageGenerator()
-    matrix = ds.variables["CO"][i,0,:,:]
-    image = image_generator.generateFromMatrix(matrix, matrix.shape)
-    images.append(image)
 
-images[0].save('test.gif', save_all=True, append_images=images)
+from ImageGenerator import ImageGenerator, ImageGeneratorMethod
+
+#print(ds.variables["time"][:])
+
+for ind_elev, elevation in tqdm(enumerate(ds.variables["lev"][:])) :
+    images = []
+    for ind_time, time in enumerate(ds.variables["time"][:]) :
+        IG = image_generator = ImageGenerator()
+        matrix = ds.variables["CO"][ind_time, ind_elev, :, :]
+        image = image_generator.generateFromMatrix(matrix, matrix.shape)
+        #image.save("test"+str(i)+".png")
+        images.append(image)
+
+    images[0].save("test" + str(elevation) + '.gif', save_all=True, append_images=images)
+

afe63ca6dded6bd0d0b98fdce0f36872139518c4
Alexandre Blais <alexandreblais42@gmail.com>
2025-10-04 14:13:49 -0400

    typo: elevation => level

--- GIFGenerator.py
+++ GIFGenerator.py
@@ -13,8 +13,8 @@ class GIFGenerator:
         self.filepath = filepath
         self.variable = variable
 
-    def setPreferedElevation(self, elevation: int):
-        self.prefered_elevation = elevation
+    def setPreferedlevel(self, level: int):
+        self.prefered_level = level
 
     def startGeneratingGifs(self):
         thread = Thread(target=self.generateGifs, args=(),)
@@ -23,9 +23,9 @@ class GIFGenerator:
     def generateGifs(self):
         self.dataset = nc.Dataset(self.filepath)
         self.variable = self.variable
-        self.elevations = self.dataset.variables["lev"][:]
-        self.elevations_to_generate: Set[int] = set(range(len(self.elevations)))
-        self.prefered_elevation = 0
+        self.levels = self.dataset.variables["lev"][:]
+        self.levels_to_generate: Set[int] = set(range(len(self.levels)))
+        self.prefered_level = 0
         self.times = self.dataset.variables["time"]
 
         self.data_matrix = self.dataset.variables[self.variable][:]
@@ -35,21 +35,21 @@ class GIFGenerator:
         self.satellite_name = self.filepath.split(".")[0]
         self.dirpath = f"{self.satellite_name}/{self.variable}/"
 
-        while self.elevations_to_generate:
-            elevation_to_generate = self.prefered_elevation
-            if self.prefered_elevation not in self.elevations_to_generate:
-                elevation_to_generate = list(self.elevations_to_generate)[0]
-            self.generateElevation(elevation_to_generate)
+        while self.levels_to_generate:
+            level_to_generate = self.prefered_level
+            if self.prefered_level not in self.levels_to_generate:
+                level_to_generate = list(self.levels_to_generate)[0]
+            self.generateLevel(level_to_generate)
 
-    def generateElevation(self, elevation_index: int):
-        self.elevations_to_generate.remove(elevation_index)
+    def generateLevel(self, level_index: int):
+        self.levels_to_generate.remove(level_index)
         images = []
         for i in range(len(self.times)):
-            matrix = self.data_matrix[i, elevation_index, :, :]
+            matrix = self.data_matrix[i, level_index, :, :]
             image = self.image_generator.generateFromMatrix(matrix, self.data_maximum, self.data_minimum)
             images.append(image)
 
-        filename = f"elevation-of-{self.elevations[elevation_index]}.gif"
+        filename = f"level-of-{self.levels[level_index]}.gif"
 
         Path(self.dirpath).mkdir(parents=True, exist_ok=True)
         images[0].save(self.dirpath + filename, save_all=True, append_images=images, loop=0)

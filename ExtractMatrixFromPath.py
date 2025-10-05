77aa65394c18cba222cb94586c7023b8649ef761
Alexandre Blais <alexandreblais42@gmail.com>
2025-10-05 15:44:36 -0400

    Optimized sub-optimal code

--- ExtractMatrixFromPath.py
+++ ExtractMatrixFromPath.py
@@ -1,37 +1,29 @@
 import netCDF4 as nc
-from ImageGenerator import ImageGenerator, ImageGeneratorMethod
 from pathlib import Path
-import numpy as np
 import os
 
 
-def extractMatrix(satellite, folder, path, analysed_stuff):
-    """
-    satellite : String
-    folder : bool
-    path : Path
-    analysed_stuff : String
-    """
+def extractMatrix(satellite: str, folder: bool, path: Path, analysed_stuff: str):
     matrices = []
     paths = []
-    if folder:
-        directory = satellite+"_DATA"
-        for entry in os.scandir(directory):
-            if entry.is_file():
-                paths.append(entry.path)
-        dss = []
-        for path in paths:
-            dss.append(nc.Dataset(path))
-        liste = []
-        for v in dss[0].variables.keys():
-            if v.isupper():
-                liste.append(v)
-        assert(len(dss[v].variables[analysed_stuff][:].shape) == 2) # :(
-        for v in range(len(liste)):
-            matrices.append(dss[v].variables[analysed_stuff][:])
-    else:
+    if not folder:
         ds = nc.Dataset(path)
         return ds.variables[analysed_stuff][:]
+
+    directory = satellite+"_DATA"
+    for entry in os.scandir(directory):
+        if entry.is_file():
+            paths.append(entry.path)
+    dss = []
+    for path in paths:
+        dss.append(nc.Dataset(path))
+    liste = []
+    for v in dss[0].variables.keys():
+        if v.isupper():
+            liste.append(v)
+        assert(len(dss[v].variables[analysed_stuff][:].shape) == 2) # :(
+        matrices.append(dss[v].variables[analysed_stuff][:])
+
     return matrices
 
-print(extractMatrix("MERRA-2", False, "MERRA-2.nc4", "CO"))
+print(extractMatrix("MERRA-2", False, Path("MERRA-2.nc4"), "CO"))
